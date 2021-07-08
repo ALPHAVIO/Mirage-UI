04eeb644d84d5e3913da11def25145f2d677f68a
Ujjawal Sharma <ujjawal.sharma.20@gmail.com>
2021-07-08 23:46:03 +0530

    Added Accordion Component

---
+++ React/accordion-component/src/components/accordion.js
@@ -0,0 +1,20 @@
+
+import './accordion.css';
+import React, { useState } from 'react';
+
+function Accordion({title,content}) {
+
+const [isActive,setActive]=useState(false);
+  return (
+    <div className="accordion-item">
+        <div className='accordion-title' onClick={() => setActive(!isActive)}> 
+            <div>{title}</div>
+            <div>{isActive?'-':'+'}</div>
+        </div>
+        {isActive && <div className="accordion-content">{content}</div>}
+      
+    </div>
+  );
+}
+
+export default Accordion;
