5849776c74bc618dad0286b019166b98d505982b
Aanchal Rakheja <68388581+aanchalrakheja@users.noreply.github.com>
2021-10-22 11:08:49 +0530

    Add Fade-up animation profile-card (#216)
    * animated-profile-card react component added
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

---
+++ React/Card/animated-profile-cards/README.md
@@ -0,0 +1,36 @@
+# Animated Profile Card
+Animates Profile Card Component that shows social-media links on card-hover.
+
+## Demo -
+https://user-images.githubusercontent.com/68388581/138270490-ef793387-2e8c-4be9-8672-1008d35ee9a7.mp4
+
+## Environment Setup-
+
+* Drop a :star: on the GitHub repository.  
+
+* Run this command to install all dependencies for the project.
+```
+yarn install
+```  
+**If you are using `npm`, you can delete the `yarn.lock` file and run `npm install`. That will add the `package-lock.json` to your folder.**  
+
+* Run this command to start the project.
+```
+yarn start
+```  
+or 
+```
+npm start
+```  
+for those using `npm`.  
+
+* Open link to view the website in your browser window if it doesn't open automatically.
+```
+http://localhost:3000/
+```  
+
+## Author
+
+[Aanchal Rakheja](https://github.com/aanchalrakheja)
+
+
---
+++ React/Card/animated-profile-cards/src/Components/profile_cards.jsx
@@ -0,0 +1,25 @@
+import "./profile_card.css";
+import profileImg from "../assets/profile_img.png";
+
+function ProfileCard()
+{  
+    return(
+              <div className="profile-card">
+                <div className="profile-image-container">
+                  <div className="profile-image">
+                    <img src={profileImg} alt="profile"></img>
+                  </div>
+                </div>
+                <div className="profile-details">
+                  <div className="name">Aanchal Rakheja</div> 
+                  <div className="post">MEMBER</div>
+                </div>
+                <div className="links">
+                    <a href="#" target="_blank" rel="noreferrer"><i className="fab fa-github fa-2x"></i></a>
+                    <a href="#" target="_blank" rel="noreferrer"><i className="fab fa-linkedin fa-2x"></i></a>
+                    <a href="#" target="_blank" rel="noreferrer"><i className="fab fa-twitter fa-2x"></i></a>
+                </div>
+              </div>
+    );
+}
+export default ProfileCard;
